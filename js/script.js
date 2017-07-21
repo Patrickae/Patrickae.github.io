936bd154310daac56f9c8eba6720e2f223685697
Andy <Andy@Admins-MacBook-Air.local>
2017-07-21 09:24:09 -0400

    commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// console.log("hello, world");
+
+// $( document ).ready(function(){
+
+// 	$(".button-collapse").sideNav();
+// 	$('.parallax').parallax();
+// 	$(".dropdown-button").dropdown();
+
+
+// })
