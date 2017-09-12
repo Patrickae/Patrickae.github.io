20d60a07355148a110976fa526b00fb46fd286bf
Andy <andyepatrick@gmail.com>
2017-09-11 23:52:05 -0400

    slide in effect

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
-// console.log("hello, world");
+ function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
 
-// $( document ).ready(function(){
 
-// 	$(".button-collapse").sideNav();
-// 	$('.parallax').parallax();
-// 	$(".dropdown-button").dropdown();
+    const portfolioElements = document.querySelectorAll(".portfolio-element");
+    console.log(portfolioElements);
+
+    function checkSlide(e){
+
+      portfolioElements.forEach(portfolioElement=>{
+        //half way through image
+        const slideInAt = (window.scrollY + window.innerHeight) -
+         portfolioElement.style.height / 2 ;
+
+         //bottom of the image
+        const imageBottom  = portfolioElement.offsetTop + portfolioElement.style.height;
+
+        const isHalfShown = slideInAt > portfolioElement.offsetTop;
+        const isNotScrolledPassed = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPassed){
+          portfolioElement.classList.add("active")
+        };
+        
+
+        console.log(portfolioElement.style);
+      })
+
+    }
+
+    window.addEventListener("scroll", debounce(checkSlide));
 
 
-// })
+
